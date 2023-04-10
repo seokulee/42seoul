83da0b50fff3a8e05c606a53ddc34747825ef05a
seokudev <cd1004s@gmail.com>
2023-04-11 01:01:01 +0900

    gnl another version - need to check some errors

--- get_next_line_secondtry/get_next_line.c
+++ get_next_line_secondtry/get_next_line.c
@@ -1,100 +1,82 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: seokklee <seokklee@student.42seoul.kr>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/27 16:26:24 by seokklee          #+#    #+#             */
-/*   Updated: 2023/04/06 20:40:36 by seokklee         ###   ########seoul.kr  */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
-
-#include <stdio.h>
-#include <fcntl.h>
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
-void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strjoin_free(char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	ft_bzero(void *s, size_t n);
+char	*get_line(int fd, char **backup);
+size_t	find_nl_idx(char *backup_fd);
 
 char	*get_next_line(int fd)
 {
+	static char	*backup[257];
 	char		*line;
-	char		*buffer;
-	static char	remainder[BUFFER_SIZE + 1];
-	char		*nl_ptr;
-	ssize_t		read_size;
-	char		*sub;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = ft_strdup("");
-	if (!line)
-		return (NULL);
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-	{
-		free(line);
-		return (NULL);
-	}
-	if (*remainder)
-		{
-			nl_ptr = ft_strchr(remainder, '\n');
-			if (nl_ptr)
-			{
-				sub = ft_substr(remainder, 0, nl_ptr - remainder + 1);
-				line = ft_strjoin_free(line, sub);
-				ft_strlcpy(remainder, nl_ptr + 1, BUFFER_SIZE - (nl_ptr - remainder));
-				free(sub);
-				return (line);
-			}
-			line = ft_strdup(remainder);
-			ft_bzero(remainder, BUFFER_SIZE + 1);
-	}
-	while (1)
-	{
-		if (0 < (read_size = read(fd, buffer, BUFFER_SIZE)))
-		{
-			nl_ptr = ft_strchr(buffer, '\n');
-			if (nl_ptr)
-			{
-				sub = ft_substr(buffer, 0, nl_ptr - buffer + 1);
-				line = ft_strjoin_free(line, sub);
-				ft_strlcpy(remainder, nl_ptr + 1, BUFFER_SIZE - (nl_ptr - buffer));
-				free(sub);
-				break ;
-			}
-			else if (read_size < BUFFER_SIZE)
-			{
-				sub = ft_substr(buffer, 0, read_size);
-				line = ft_strjoin_free(line, sub);
-				free(sub);
-				break ;
-			}
-			else
-				line = ft_strjoin_free(line, buffer);
-			ft_bzero(buffer, BUFFER_SIZE + 1);
-		}
-		else
-			break ;
-
-	}
-	free(buffer);
-	if (!*line)
-	{
-		free(line);
-		return (NULL);
-	}
+	line = get_line(fd, backup);
 	return (line);
 }
 
+char	*get_line(int fd, char **backup)
+{
+	ssize_t	read_size;
+	char	*buffer;
+	char	*tmp;
+	char	*line;
+	size_t	idx;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	if (!backup[fd])
+		backup[fd] = ft_strdup("");
+	read_size = 1;
+	while (!ft_strchr(buffer, '\n') && read_size)
+	{
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		tmp = backup[fd];
+		backup[fd] = ft_strjoin(tmp, buffer);
+		free(tmp);
+	}
+	if (ft_strchr(backup[fd], '\n'))
+	{
+		idx = find_nl_idx(backup[fd]);
+		line = ft_substr(backup[fd], 0, idx + 1);
+		tmp = backup[fd];
+		backup[fd] = ft_strdup(&(backup[fd])[idx + 1]);
+		free(tmp);
+		return (line);
+	}
+	line = ft_strdup(backup[fd]);
+	free(backup[fd]);
+	free(buffer);
+	return (line);
+}
+
+size_t	find_nl_idx(char *backup_fd)
+{
+	size_t	idx;
+
+	idx = 0;
+	if (!backup_fd)
+		return (0);
+	while(backup_fd[idx] != '\n')
+		idx++;
+	return (idx);
+}
+
+//#include <stdio.h>
+//#include <fcntl.h>
 // int	main()
 // {
 // 	int	fd;
@@ -111,10 +93,9 @@ char	*get_next_line(int fd)
 // 	}
 // 	else
 // 		printf("fail\n");
-// 	// system("leaks a.out");
+// 	// system("leaks test");
 // 	return 0;
 // }
-
 
 char	*ft_strdup(const char *s1)
 {
@@ -175,15 +156,6 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*uc;
-
-	uc = s;
-	while (n--)
-		*uc++ = 0;
-}
-
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
@@ -201,7 +173,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	return (src_len);
 }
 
-char	*ft_strjoin_free(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	size_t	s1_len;
@@ -213,13 +185,9 @@ char	*ft_strjoin_free(char *s1, char *s2)
 	s2_len = ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * s1_len + s2_len + 1);
 	if (!str)
-	{
-		free(s1);
 		return (NULL);
-	}
 	ft_strlcpy(str, s1, s1_len + 1);
 	ft_strlcat(str, s2, s1_len + s2_len + 1);
-	free(s1);
 	return (str);
 }
 
@@ -236,8 +204,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (len > s_len - start)
 		len = s_len - start;
 	ret = (char *)malloc(sizeof(char) * (len + 1));
-	if (ret == NULL)
+	if (!ret)
 		return (NULL);
 	ft_strlcpy(ret, s, len + 1);
 	return (ret);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*uc;
+
+	uc = s;
+	while (n--)
+		*uc++ = 0;
 }

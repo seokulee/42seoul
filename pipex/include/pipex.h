88331815792f2390d145a8e0b7de7b05feb90400
seokklee <cd1004s@gmail.com>
2023-06-02 16:09:53 +0900

    pipex begin

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -4,7 +4,17 @@
 /*--dependecies--*/
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <stdio.h>
+# include <string.h>
+
+# define ERR_INFILE "Infile"
+# define ERR_OUTFILE "Outfile"
+# define ERR_INPUT "Invalid number of arguments.\n"
+# define ERR_PIPE "Pipe"
+# define ERR_CMD "Command not found\n"
 
 #endif
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -1,22 +1,6 @@
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	int	mode;
-
-	(void) argc;
-	(void) argv;
-	mode = 0;
-	mode |= F_OK;
-	if (!access("test.txt", mode))
-		printf("File is existing\n");
-	else
-		printf("File is not existing\n");
-	mode &= 0;
-	mode |= X_OK;
-	if (!access("test.txt", mode))
-		printf("File is excutable\n");
-	else
-		printf("File is not excutable\n");
-	return (0);
+	
 }

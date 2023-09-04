f4e95e6355596946a17350d53ec8aac4fa2399f2
seokuMac <cd1004s@gmail.com>
2023-09-04 14:51:18 +0900

    get_next_line

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -7,12 +7,12 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-char	*get_next_line_gnl(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_get_line(int fd, char **backup, char *buffer);
 size_t	ft_find_nl_idx(char *backup_fd);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin_gnl(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_seperate(int fd, char **backup);
--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -1,99 +1,114 @@
 #include "get_next_line_bonus.h"
 
-char	*ft_strjoin_gnl(char *s1, char *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen(const char *s);
+char	*ft_get_line(int fd, char **backup, char *buffer);
+size_t	ft_find_nl_idx(char *backup_fd);
+char	*ft_seperate(int fd, char **backup);
+char	*ft_eof(int fd, char **backup);
 
-char	*ft_strjoin_gnl(char *s1, char *s2)
+char	*get_next_line(int fd)
 {
-	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	i;
+	static char	*backup[OPEN_MAX];
+	char		*line;
+	char		*buffer;
 
-	if (!s1 || !s2)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * s1_len + s2_len + 1);
-	if (!str)
+	if (!backup[fd])
 	{
-		free(s1);
+		backup[fd] = ft_strdup("");
+		if (!backup[fd])
+			return (0);
+	}
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+	{
+		free(backup[fd]);
+		backup[fd] = NULL;
 		return (NULL);
 	}
-	i = -1;
-	while (++i < s1_len)
-		str[i] = s1[i];
-	i--;
-	while (++i < s1_len + s2_len)
-		str[i] = s2[i - s1_len];
-	str[i] = '\0';
-	free(s1);
-	return (str);
+	line = ft_get_line(fd, backup, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!backup[fd])
+		free(backup[fd]);
+	return (line);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_get_line(int fd, char **backup, char *buffer)
 {
-	size_t	s_len;
-	char	*ret;
-	size_t	i;
+	ssize_t	read_size;
+	char	*line;
 
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (s_len <= start)
-		return (ft_strdup(""));
-	if (len > s_len - start)
-		len = s_len - start;
-	ret = (char *)malloc(sizeof(char) * (len + 1));
-	if (!ret)
-		return (NULL);
-	i = 0;
-	while (i < len)
-		ret[i++] = *s++;
-	ret[i] = '\0';
-	return (ret);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*str;
-	size_t	count;
-
-	if (!s1)
-		return (NULL);
-	count = ft_strlen(s1);
-	str = (char *)malloc(sizeof(char) * (count + 1));
-	if (!str)
-		return (NULL);
-	while (*s1)
-		*str++ = *s1++;
-	*str = '\0';
-	str -= count;
-	return (str);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (*s++)
-		len++;
-	return (len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
+	read_size = 1;
+	while (!ft_strchr(backup[fd], '\n') && read_size)
 	{
-		if (*s == (unsigned char) c)
-			return ((char *) s);
-		s++;
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size < 0)
+		{
+			free(backup[fd]);
+			backup[fd] = NULL;
+			return (NULL);
+		}
+		buffer[read_size] = '\0';
+		backup[fd] = ft_strjoin_gnl(backup[fd], buffer);
+		if (!backup[fd])
+			return (NULL);
 	}
-	if (*s == (unsigned char) c)
-		return ((char *) s);
-	return (NULL);
+	if (ft_strchr(backup[fd], '\n'))
+		line = ft_seperate(fd, backup);
+	else
+		line = ft_eof(fd, backup);
+	return (line);
+}
+
+char	*ft_seperate(int fd, char **backup)
+{
+	size_t	idx;
+	char	*tmp;
+	char	*line;
+
+	idx = ft_find_nl_idx(backup[fd]);
+	line = ft_substr(backup[fd], 0, idx + 1);
+	if (!line)
+	{
+		free(backup[fd]);
+		backup[fd] = NULL;
+		return (NULL);
+	}
+	tmp = backup[fd];
+	backup[fd] = ft_strdup(&(backup[fd])[idx + 1]);
+	free(tmp);
+	if (!backup[fd])
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+char	*ft_eof(int fd, char **backup)
+{
+	char	*line;
+
+	line = ft_strdup(backup[fd]);
+	free(backup[fd]);
+	backup[fd] = NULL;
+	if (!line || !*line)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+size_t	ft_find_nl_idx(char *backup_fd)
+{
+	size_t	idx;
+
+	idx = 0;
+	if (!backup_fd)
+		return (0);
+	while (backup_fd[idx] != '\n')
+		idx++;
+	return (idx);
 }

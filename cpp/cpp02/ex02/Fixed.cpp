74d60e2c6128d770d512941c25b1a369337fa795
seokulee <cd1004s@gmail.com>
2024-04-10 15:16:40 +0900

    cpp: cpp02 -> ex02

---
+++ cpp/cpp02/ex02/Fixed.cpp
@@ -0,0 +1,142 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : mValue(0) {}
+
+Fixed::~Fixed() {}
+
+Fixed::Fixed(int const value) : mValue(value << mFractionalBits) {}
+
+Fixed::Fixed(float const value) : mValue(static_cast<int>(roundf(value * (1 << mFractionalBits)))) {}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+    *this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+    mValue = fixed.getRawBits();
+    return *this;
+}
+
+bool Fixed::operator>(const Fixed &fixed) const
+{
+    return mValue > fixed.getRawBits() ? true : false;
+}
+
+bool Fixed::operator<(const Fixed &fixed) const
+{
+    return mValue < fixed.getRawBits() ? true : false;
+}
+
+bool Fixed::operator>=(const Fixed &fixed) const
+{
+    return mValue >= fixed.getRawBits() ? true : false;
+}
+
+bool Fixed::operator<=(const Fixed &fixed) const
+{
+    return mValue <= fixed.getRawBits() ? true : false;
+}
+
+bool Fixed::operator==(const Fixed &fixed) const
+{
+    return mValue == fixed.getRawBits() ? true : false;
+}
+
+bool Fixed::operator!=(const Fixed &fixed) const
+{
+    return mValue != fixed.getRawBits() ? true : false;
+}
+
+Fixed Fixed::operator+(const Fixed &fixed) const
+{
+    return Fixed(toFloat() + fixed.toFloat());
+}
+
+const Fixed Fixed::operator++(int)
+{
+    Fixed tmp(*this);
+    mValue++;
+    return tmp;
+}
+
+Fixed &Fixed::operator++()
+{
+    mValue++;
+    return *this;
+}
+
+Fixed Fixed::operator-(const Fixed &fixed) const
+{
+    return Fixed(toFloat() - fixed.toFloat());
+}
+
+Fixed &Fixed::operator--()
+{
+    mValue--;
+    return *this;
+}
+
+const Fixed Fixed::operator--(int)
+{
+    Fixed tmp(*this);
+    mValue--;
+    return tmp;
+}
+
+Fixed Fixed::operator*(const Fixed &fixed) const
+{
+    return Fixed(toFloat() * fixed.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &fixed) const
+{
+    return Fixed(toFloat() / fixed.toFloat());
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &fixed)
+{
+    out << fixed.toFloat();
+    return out;
+}
+
+int Fixed::getRawBits(void) const
+{
+    return mValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    mValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(mValue) / (1 << mFractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return mValue >> mFractionalBits;
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+    return a < b ? a : b;
+}
+
+Fixed const &Fixed::min(Fixed const &a, Fixed const &b)
+{
+    return a < b ? a : b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{
+    return a > b ? a : b;
+}
+
+Fixed const &Fixed::max(Fixed const &a, Fixed const &b)
+{
+    return a > b ? a : b;
+}
---
+++ cpp/cpp02/ex02/Fixed.hpp
@@ -0,0 +1,43 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+    int mValue;
+    static const int mFractionalBits = 8;
+public:
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed &fixed);
+    Fixed(int const value);
+    Fixed(float const value);
+    Fixed &operator=(const Fixed &fixed);
+    bool operator>(const Fixed &fixed) const;
+    bool operator<(const Fixed &fixed) const;
+    bool operator>=(const Fixed &fixed) const;
+    bool operator<=(const Fixed &fixed) const;
+    bool operator==(const Fixed &fixed) const;
+    bool operator!=(const Fixed &fixed) const;
+    Fixed operator+(const Fixed &fixed) const;
+    Fixed operator-(const Fixed &fixed) const;
+    Fixed operator*(const Fixed &fixed) const;
+    Fixed operator/(const Fixed &fixed) const;
+    const Fixed operator++(int);
+    Fixed &operator++();
+    const Fixed operator--(int);
+    Fixed &operator--();
+    friend std::ostream &operator<<(std::ostream &out, Fixed const &fixed);
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat(void) const;
+    int toInt(void) const;
+    static Fixed &min(Fixed &a, Fixed &b);
+    static Fixed const &min(Fixed const &a, Fixed const &b);
+    static Fixed &max(Fixed &a, Fixed &b);
+    static Fixed const &max(Fixed const &a, Fixed const &b);
+};
+
+#endif

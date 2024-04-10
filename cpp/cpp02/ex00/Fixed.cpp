ae59c1b78b4414354ae5e6f2e631b514a9580db5
seokulee <cd1004s@gmail.com>
2024-04-10 11:41:29 +0900

    cpp: cpp02 -> ex00

---
+++ cpp/cpp02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : mValue(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    mValue = fixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return mValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    mValue = raw;
+}
